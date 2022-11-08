25404dbdca4e7296fd2b9942f569c1732ab4cb09
Vedat Sözen <vedatsozen@gmail.com>
2022-11-08 21:20:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const bgArray = [
+    "photo1.jpg","photo2.jpg","photo3.jpg"
+];
+
+document.body.style.backgroundImage = "url(" + bgArray[0] + ")";
+document.body.style.backgroundSize = "cover" ;
+document.body.style.backgroundRepeat = "no-repeat"
+
+function bg1() {
+
+document.body.style.backgroundImage = "url(" + bgArray[0] + ")";
+document.body.style.backgroundSize = "cover" ;
+document.body.style.backgroundRepeat = "no-repeat"
+
+}
+
+function bg2() {
+
+    document.body.style.backgroundImage = "url(" + bgArray[1] + ")";
+    document.body.style.backgroundSize = "cover" ;
+    document.body.style.backgroundRepeat = "no-repeat"
+    
+    }
+
+    function bg3() {
+
+        document.body.style.backgroundImage = "url(" + bgArray[2] + ")";
+        document.body.style.backgroundSize = "cover" ;
+        document.body.style.backgroundRepeat = "no-repeat"
+        
+        }
+    
